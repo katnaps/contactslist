703dd93421f6b9229527773a7dc833fdd1f101ea
Azri <59246759+katnaps@users.noreply.github.com>
2021-02-16 12:08:28 +0800

    switch to useState

--- src/contexts/ContactContext.js
+++ src/contexts/ContactContext.js
@@ -1,15 +1,9 @@
 import React, { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
-import { ContactReducer } from '../reducer/ContactReducer';
 
 export const ContactContext = createContext();
 
 const ContactContextProvider = (props) => {
-    // const [contacts, dispatch] = useReducer(ContactReducer, [], () => {
-    //     const localData = localStorage.getItem('contacts');
-    //     return localData ? JSON.parse(localData) : [];
-    // });
-
     const [contacts, setContacts] = useState([]);
 
     useEffect(() => {
@@ -18,20 +12,6 @@ const ContactContextProvider = (props) => {
             setContacts(response.data)
         })
     }, [contacts.id])
-
-    console.log(contacts)
-
-    // useEffect(() => {
-    //     axios.get('https://contactsbookapi.herokuapp.com/api/contacts')
-    //         .then(response => {
-    //             dispatch({
-    //                 type: 'FETCH_SUCCESS', payload: response.data
-    //             })
-    //         })
-    //     localStorage.setItem('contacts', JSON.stringify(contacts));
-
-    // }, [contacts.id]);
-
 
     return (
         <ContactContext.Provider value={{ contacts, setContacts }}>
